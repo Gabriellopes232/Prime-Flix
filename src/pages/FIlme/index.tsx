292608952e2bdbb1f4c173b053822b449a63b138
Gabriellopes232 <xdynatz@gmail.com>
2022-06-03 15:39:54 -0300

    feature: page my movies

--- src/pages/FIlme/index.tsx
+++ src/pages/FIlme/index.tsx
@@ -1,6 +1,29 @@
-import React from 'react'
+import { useState, useEffect } from "react"
+import { useParams } from "react-router-dom"
+
+import { api } from '../../services/api'
 
 export default function Filme() {
+    const { id } = useParams()
+    const [filme, setFilme] = useState({})
+    const [loading, setLoading] = useState(true)
+    useEffect(() => {
+        async function loadFilme() {
+            await api.get(`/movie/${id}`, {
+                params: {
+                    api_key: "84932e9c944ef8b57e5cff1ea6cf3590",
+                    language: "pt_BR",
+                }
+            })
+                .then((response) => {
+                    setFilme(response.data)
+                })
+                .catch(() => {
+
+                })
+        }
+        loadFilme()
+    }, [])
     return (
         <div>Bem vindo a pagina de filmes</div>
     )
